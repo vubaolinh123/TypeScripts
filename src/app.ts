1fb7f27b0d4bceb33b05872ff0acaeea382baaf6
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-03-08 18:11:14 +0700

    Update buổi 1

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+export {}
+type User = {
+    id: number;
+    name: string;
+}
+
+const myName: string = "Vu Bao Linh";
+const myAge: number = 18;
+const myObj: object = {};
+
+const numberArr: number[] = [1,2,3,4];
+const stringArr: string[] = ["a","b","c"];
+const objectArr: User[] = [{id:1,name:"Linh"},{id:2,name:"Vu"}]
+
+function sum(a: number, b: number):number{
+    return a + b;
+}
+
+sum(10,5)
+
+
+
+
